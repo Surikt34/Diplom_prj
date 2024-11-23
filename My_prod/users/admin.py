7e44ff53e67187d1c59aa6d6a51dec8c303bef8d
Surikt34 <surikt34@gmail..com>
2024-11-24 00:56:15 +0500

    JWT и админка пользователя

--- My_prod/users/admin.py
+++ My_prod/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительные поля', {
+            'fields': ('role', 'phone', 'address', 'date_of_birth', 'avatar', 'is_verified'),
+        }),
+    )
+    list_display = ['username', 'email', 'role', 'is_verified']
+    list_filter = ['role', 'is_verified']
