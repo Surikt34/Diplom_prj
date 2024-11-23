5f06af59a3bcc2e2fc1ff428165423e0109d5128
Surikt34 <surikt34@gmail..com>
2024-11-24 00:52:47 +0500

    маршруты пользователя

--- My_prod/My_prod/urls.py
+++ My_prod/My_prod/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/catalog/', include('catalog.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/users/', include('users.urls')),
 ]
