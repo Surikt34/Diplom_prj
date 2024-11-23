9754c19a311ff732932648287cc1f2bcec29ec60
Surikt34 <surikt34@gmail..com>
2024-11-23 23:05:17 +0500

    добавление моделей каталога в админ панель

--- My_prod/catalog/admin.py
+++ My_prod/catalog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Product, Supplier, ProductAttribute, ProductImage
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'description')
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'updated_at')
+    list_filter = ('category', 'suppliers')
+    search_fields = ('name', 'description')
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'is_active')
+    search_fields = ('name', 'email')
+
+@admin.register(ProductAttribute)
+class ProductAttributeAdmin(admin.ModelAdmin):
+    list_display = ('product', 'name', 'value')
+    list_filter = ('name',)
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'is_main')
