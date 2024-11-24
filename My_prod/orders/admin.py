7d1e654c196964ef2646d047cec60ef9c2556997
Surikt34 <surikt34@gmail..com>
2024-11-24 18:31:59 +0500

    админка orders

--- My_prod/orders/admin.py
+++ My_prod/orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'status', 'total_price', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['user__username']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price']
+    search_fields = ['product__name']
