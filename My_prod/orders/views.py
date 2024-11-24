95aa494db246edebb6114f0656a7bc1d26258f01
Surikt34 <surikt34@gmail..com>
2024-11-24 18:14:33 +0500

    views orders

--- My_prod/orders/views.py
+++ My_prod/orders/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, CreateOrderSerializer
 
-# Create your views here.
+class OrderListView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Клиенты видят только свои заказы
+        return self.queryset.filter(user=self.request.user)
+
+class OrderDetailView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+class CreateOrderView(generics.CreateAPIView):
+    serializer_class = CreateOrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
