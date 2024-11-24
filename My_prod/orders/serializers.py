5e4f7f4f826163efda6400a2055c6aa7fc0d2d37
Surikt34 <surikt34@gmail..com>
2024-11-24 17:58:19 +0500

    serializers orders

---
+++ My_prod/orders/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+from catalog.models import Product
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.ReadOnlyField(source='product.name')
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'product_name', 'quantity', 'price']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'status', 'created_at', 'updated_at', 'total_price', 'items']
+
+class CreateOrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'status', 'items']
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+        return order
